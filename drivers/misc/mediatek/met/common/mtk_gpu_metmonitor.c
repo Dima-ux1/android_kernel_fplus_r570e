148c44a10ec40fafe4deddb8ae5b4d917c6ca5ce
Dima-ux1 <eugenetsvetnov@yandex.ru>
2025-12-30 14:33:58 +0300

    Update mtk_gpu_metmonitor.c

--- drivers/misc/mediatek/met/common/mtk_gpu_metmonitor.c
+++ drivers/misc/mediatek/met/common/mtk_gpu_metmonitor.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 MediaTek Inc.
+ * Copyright (C) 2019 MediaTek Inc. 1
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
